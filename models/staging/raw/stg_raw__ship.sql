dd77793cf1ef23cc3a4f4e6b46df611925d290cf
sarah.rhomri@gmail.com <sarah.rhomri@gmail.com>
2025-11-04 11:54:08 +0000

    part 2

---
+++ models/staging/raw/stg_raw__ship.sql
@@ -0,0 +1,21 @@
+with 
+
+source as (
+
+    select * from {{ source('raw', 'ship') }}
+
+),
+
+renamed as (
+
+    select
+        orders_id,
+        shipping_fee,
+        logcost,
+        CAST (ship_cost AS float64)
+
+    from source
+
+)
+
+select * from renamed
