9ee3e0b06553e31d5f272de4c153a307fcfee4af
sarah.rhomri@gmail.com <sarah.rhomri@gmail.com>
2025-11-05 11:44:11 +0000

    test

--- models/staging/raw/stg_raw__ship.sql
+++ models/staging/raw/stg_raw__ship.sql
@@ -11,7 +11,7 @@ renamed as (
     select
         orders_id,
         shipping_fee,
-        logcost,
+        logcost as log_cost,
         CAST (ship_cost AS float64) AS ship_cost
 
     from source
